7ca38f835bb126887f754a80dfdb188d6be5f141
seetharamn <seetharamprasad.n@gmail.com>
2022-04-28 21:36:06 -0700

    Test Cases Added and Verified

--- src/test/java/com/retailer/rewards/RewardsApplicationTests.java
+++ src/test/java/com/retailer/rewards/RewardsApplicationTests.java
@@ -1,55 +1,46 @@
 package com.retailer.rewards;
 
-import java.io.IOException;
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.retailer.rewards.dao.PurchaseDAO;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = RewardsApplication.class)
 @WebAppConfiguration
+@AutoConfigureMockMvc
 class RewardsApplicationTests {
 	
-	protected MockMvc mvc;
-
 	@Autowired
 	WebApplicationContext webApplicationContext;
 	
 	@Autowired
 	ObjectMapper objectMapper;
 	
+	@Autowired
+	protected MockMvc mvc;
+	
 	@MockBean
-	protected PurchaseDAO purchaseDAO;
-
+	@Autowired
+	PurchaseDAO purchaseDAO;
+	
 	protected void setUp() {
-		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		//mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
-
-	protected String mspToJson(Object obj) throws JsonProcessingException {
-		return objectMapper.writeValueAsString(obj);
-	}
-
-	protected <T> T mapFromJson(String json, Class<T> clazz)
-			throws JsonParseException, JsonMappingException, IOException {
-		return objectMapper.readValue(json, clazz);
-	}
-
+	
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
--- src/test/java/com/retailer/rewards/RewardsTest.java
+++ src/test/java/com/retailer/rewards/RewardsTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,24 +12,38 @@ import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.retailer.rewards.bo.RewardsCalculator;
 import com.retailer.rewards.model.Purchase;
 import com.retailer.rewards.model.Rewards;
 
-public class RewardControllerTest extends RewardsApplicationTests {
+public class RewardsTest extends RewardsApplicationTests{
 
+	
+	
 	RewardsCalculator rewardsCalculator = new RewardsCalculator();
-
+	
 	@Override
 	@Before
 	public void setUp() {
 		super.setUp();
+	}
+
+	protected String mspToJson(Object obj) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(obj);
+	}
+
+	protected <T> T mapFromJson(String json, Class<T> clazz)
+			throws JsonParseException, JsonMappingException, IOException {
+		return objectMapper.readValue(json, clazz);
 	}
 
 	@Test
@@ -81,7 +96,7 @@ public class RewardControllerTest extends RewardsApplicationTests {
 		List<Purchase> purchases = Arrays.asList(p, p1, p2);
 		Mockito.when(purchaseDAO.saveAll(purchases)).thenReturn(purchases);
 
-		String inputJson = super.mspToJson(purchases);
+		String inputJson = mspToJson(purchases);
 		MvcResult result = mvc.perform(
 				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
 				.andReturn();
@@ -105,7 +120,7 @@ public class RewardControllerTest extends RewardsApplicationTests {
 		p2.setCustomerId("C002");
 		p2.setTransactionDate(new Date(System.currentTimeMillis()));
 
-		Mockito.when(purchaseDAO.findByCustomerId("COO2")).thenReturn(Arrays.asList(p, p2));
+		Mockito.when(purchaseDAO.findByCustomerId("C002")).thenReturn(Arrays.asList(p, p2));
 		MvcResult result = mvc
 				.perform(MockMvcRequestBuilders.get("/api/rewards/C002").accept(MediaType.APPLICATION_JSON_VALUE))
 				.andReturn();
@@ -152,7 +167,7 @@ public class RewardControllerTest extends RewardsApplicationTests {
 		p.setTransactionDate(new Date(System.currentTimeMillis()));
 		Mockito.when(purchaseDAO.findByCustomerId("C001")).thenReturn(Arrays.asList(p));
 		Rewards r = rewardsCalculator.calucateRewards("C001", Arrays.asList(p));
-		assertTrue(r.getTotal() == 110.60 && r.getMonthlyRewards().size()==1);
+		assertTrue(r.getTotal() == 90.60 && r.getMonthlyRewards().size()==1);
 	}
 	
 	@Test
@@ -176,10 +191,20 @@ public class RewardControllerTest extends RewardsApplicationTests {
 		p1.setAmount(120.30);
 		p1.setCustomerId("C001");
 		Calendar c=Calendar.getInstance();
-		c.set(Calendar.DAY_OF_MONTH, -1);
-		p.setTransactionDate(new Date(c.getTimeInMillis()));
+		c.set(Calendar.MONTH, -1);
+		p1.setTransactionDate(new Date(c.getTimeInMillis()));
 		Mockito.when(purchaseDAO.findByCustomerId("C001")).thenReturn(Arrays.asList(p,p1));
 		Rewards r = rewardsCalculator.calucateRewards("C001", Arrays.asList(p,p1));
-		assertTrue(r.getTotal() == 141.20 && r.getMonthlyRewards().size()==2);
+		assertTrue(r.getMonthlyRewards().size()==2);
+	}
+	
+	@Test
+	public void calculateRewardspurchasesMadeInLastThreeMonths() throws Exception{
+		String json="[{ \"customerId\":\"C001\", \"amount\":\"120.30\", \"transactionDate\":\"04/25/2022\" },{ \"customerId\":\"C001\", \"amount\":\"598.10\", \"transactionDate\":\"04/20/2022\" },{ \"customerId\":\"C001\", \"amount\":\"150.00\", \"transactionDate\":\"03/10/2022\" },{ \"customerId\":\"C001\", \"amount\":\" 50.00\", \"transactionDate\":\"03/30/2022\" },{ \"customerId\":\"C001\", \"amount\":\"50.40\", \"transactionDate\":\"03/05/2022\" },{ \"customerId\":\"C001\", \"amount\":\"72.60\", \"transactionDate\":\"02/10/2022\" },{ \"customerId\":\"C001\", \"amount\":\"117.30\", \"transactionDate\":\"04/25/2022\" },{ \"customerId\":\"C001\", \"amount\":\"99.30\", \"transactionDate\":\"04/15/2022\" },{ \"customerId\":\"C001\", \"amount\":\"1017.30\", \"transactionDate\":\"03/20/2022\" },{ \"customerId\":\"C001\", \"amount\":\"315.99\", \"transactionDate\":\"02/25/2022\" },{ \"customerId\":\"C001\", \"amount\":\"119.67\", \"transactionDate\":\"03/05/2022\" }, { \"customerId\":\"C001\", \"amount\":\"617.39\", \"transactionDate\":\"02/28/2022\" }]";
+		Purchase[] purchases=mapFromJson(json, Purchase[].class);
+		Mockito.when(purchaseDAO.findByCustomerId("C001")).thenReturn(Arrays.asList(purchases));
+		Rewards r = rewardsCalculator.calucateRewards("C001", Arrays.asList(purchases));
+		assertTrue(r.getTotal()==4984.4 && r.getMonthlyRewards().size()==3);
+	
 	}
 }
